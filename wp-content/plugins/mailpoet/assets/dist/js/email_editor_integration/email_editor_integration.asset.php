1ebc79d4a1b46645e7694d82b0fafe4868ad1d19
Nicholas Kirui <nicholaskirui6@gmail.com>
2025-01-09 13:15:43 +0300

    wc mpesa

--- wp-content/plugins/mailpoet/assets/dist/js/email_editor_integration/email_editor_integration.asset.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-if (!defined('ABSPATH')) exit;
-
- return array('dependencies' => array('jquery', 'moment', 'react', 'react-dom', 'wp-escape-html', 'wp-hooks', 'wp-i18n'), 'version' => '08ea0f1f17e03cb30350');
