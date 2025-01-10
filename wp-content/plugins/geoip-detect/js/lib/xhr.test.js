3141ed335b7fa79c7ca5c3afd626fcda10d15819
Nicholas Kirui <nicholaskirui6@gmail.com>
2025-01-10 10:19:26 +0300

    db final

--- wp-content/plugins/geoip-detect/js/lib/xhr.test.js
+++
@@ -1,6 +0,0 @@
-import { jsonDecodeIfPossible } from "./xhr";
-
-test('jsonDecode', () => {
-    expect(jsonDecodeIfPossible('{"is_empty":true}')).toStrictEqual({is_empty:true});
-    expect(jsonDecodeIfPossible('{"is_empty":true')).toStrictEqual({ is_empty: true, extra: { error: 'Invalid JSON: {"is_empty":true'} });
-});
--- wp-content/plugins/geoip-detect/js/lookup/options.test.js
+++
@@ -1,9 +0,0 @@
-/**
- * @jest-environment jsdom
- */
-
-import { options } from "./options";
-
-test('options', () => {
-    expect(options.do_body_classes).toBe(false);
-});
